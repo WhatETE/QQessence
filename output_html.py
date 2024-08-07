e68b7d2048551f85b46b831d634a1f4560a25000
DetectiveGong <eterfire@qq.com>
2024-08-07 10:08:30 +0800

    Update output_html.py
    修复消息内容中的单引号被错误替换的问题

--- output_html.py
+++ output_html.py
@@ -1,40 +1,31 @@
 import os
-import json
 
-with open('output.txt', 'r', encoding='utf-8') as file:
-    data = file.read()
+with open("output.txt", "r", encoding="utf-8") as file:
+    data = file.readlines()
 
-#替换单引号为双引号
-data = data.replace("'", "\"")
-
-with open('fixed_output.json', 'w', encoding='utf-8') as json_file:
-    json_file.write(data)
-
-with open('fixed_output.json', 'r', encoding='utf-8') as json_file:
-    data = json_file.readlines()
-
-#导出html
-html_content = '<html><head><title>QQ Group Messages</title></head><body>'
+# 导出html
+html_content = "<html><head><title>QQ Group Messages</title></head><body>"
 
 for item in data:
-    item_data = json.loads(item)
-    qq_name = item_data['qq_name']
-    content = item_data['content']
+    item_data = eval(item)
+    qq_name = item_data["qq_name"]
+    content = item_data["content"]
 
-    html_content += f'<p><b>{qq_name}:</b>'
+    html_content += f"<p><b>{qq_name}:</b>"
 
     for msg in content:
-        if msg.startswith('http'):
+        if msg.startswith("http"):
             img_name = os.path.basename(msg)
+            # 图片需要在当前目录的img文件夹下
             html_content += f'<br><img src="img/{img_name}" alt="{img_name}">'
         else:
-            html_content += f'<br>{msg}'
+            html_content += f"<br>{msg}"
 
-    html_content += '</p>'
+    html_content += "</p>"
 
-html_content += '</body></html>'
+html_content += "</body></html>"
 
-with open('messages.html', 'w', encoding='utf-8') as html_file:
+with open("messages.html", "w", encoding="utf-8") as html_file:
     html_file.write(html_content)
 
-print('HTML 文件已创建。')
+print("HTML 文件已创建。")
